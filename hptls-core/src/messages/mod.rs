ecc10e5c97190e56d3394a709db83499a60a83d7
mamonet <maamoun.tk@gmail.com>
2025-11-17 15:17:13 +0100

    Create TLS messages module structure
    Set up module organization for all TLS handshake
    message types.

---
+++ hptls-core/src/messages/mod.rs
@@ -0,0 +1,28 @@
+//! TLS protocol messages.
+//!
+//! This module contains all TLS handshake message types as defined in RFC 8446.
+
+pub mod certificate;
+pub mod certificate_request;
+pub mod certificate_verify;
+pub mod client_hello;
+pub mod encrypted_extensions;
+pub mod end_of_early_data;
+pub mod finished;
+pub mod hello_retry_request;
+pub mod key_update;
+pub mod new_session_ticket;
+pub mod server_hello;
+
+// Re-exports
+pub use certificate::{Certificate, CertificateEntry};
+pub use certificate_request::CertificateRequest;
+pub use certificate_verify::CertificateVerify;
+pub use client_hello::ClientHello;
+pub use encrypted_extensions::EncryptedExtensions;
+pub use end_of_early_data::EndOfEarlyData;
+pub use finished::Finished;
+pub use hello_retry_request::{HelloRetryRequest, HELLO_RETRY_REQUEST_RANDOM};
+pub use key_update::KeyUpdate;
+pub use new_session_ticket::NewSessionTicket;
+pub use server_hello::ServerHello;
