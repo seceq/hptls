09ce4540b8af4a422236e4ed9ddf72bf46f12f57
mamonet <maamoun.tk@gmail.com>
2025-11-19 00:50:47 +0100

    Add ACK and NEW_CONNECTION_ID message types for DTLS

--- hptls-core/src/messages/mod.rs
+++ hptls-core/src/messages/mod.rs
@@ -2,6 +2,7 @@
 //!
 //! This module contains all TLS handshake message types as defined in RFC 8446.
 
+pub mod ack;
 pub mod certificate;
 pub mod certificate_request;
 pub mod certificate_verify;
@@ -11,10 +12,13 @@ pub mod end_of_early_data;
 pub mod finished;
 pub mod hello_retry_request;
 pub mod key_update;
+pub mod new_connection_id;
 pub mod new_session_ticket;
 pub mod server_hello;
 
 // Re-exports
+pub use ack::Ack;
+pub use new_connection_id::NewConnectionId;
 pub use certificate::{Certificate, CertificateEntry};
 pub use certificate_request::CertificateRequest;
 pub use certificate_verify::CertificateVerify;
